a8c1d03455825b169c060b676e4d6de71dd68e87
years1hundred <yearsonehundred@gmail.com>
2016-11-15 00:41:20 -0500

    Finished coding auto-elevators.

--- unity-game/Assets/Scripts/ElevatorScripts/AutoElevator.cs
+++ unity-game/Assets/Scripts/ElevatorScripts/AutoElevator.cs
@@ -5,14 +5,18 @@ public class AutoElevator : MonoBehaviour
 {
     public bool notMoving;
     public bool reachedDestination;
+    public bool onStartPoint;
+    public bool onEndPoint;
 
     public GameObject objectToMove;
     public GameObject theRightWall;
     public GameObject theLeftWall;
-    public GameObject firstRightBarrier;
-    public GameObject secondRightBarrier;
-    public GameObject firstLeftBarrier;
-    public GameObject secondLeftBarrier;
+    public GameObject theStartLeftBarrier;
+    public GameObject theStartRightBarrier;
+    public GameObject theEndLeftBarrier;
+    public GameObject theEndRightBarrier;
+    public GameObject theStartPoint;
+    public GameObject theEndPoint;
 
     public Transform startPoint;
     public Transform endPoint;
@@ -29,6 +33,13 @@ public class AutoElevator : MonoBehaviour
         currentTarget = endPoint.position;
 
         waitCounter = waitTime;
+
+        theLeftWall.SetActive(false);
+        theRightWall.SetActive(false);
+        theStartLeftBarrier.SetActive(false);
+        theStartRightBarrier.SetActive(false);
+        theEndLeftBarrier.SetActive(false);
+        theEndRightBarrier.SetActive(false);
     }
 
 
@@ -37,36 +48,110 @@ public class AutoElevator : MonoBehaviour
     {     
         if (objectToMove.transform.position == endPoint.position)
         {
+            onStartPoint = false;
+            onEndPoint = true;
             if (waitCounter < 0f)
             {
                 reachedDestination = true;
+                theStartLeftBarrier.SetActive(false);
+                theStartRightBarrier.SetActive(false);
+                theEndLeftBarrier.SetActive(false);
+                theEndRightBarrier.SetActive(false);
             }
             notMoving = true;
             currentTarget = startPoint.position;
-            theLeftWall.SetActive(false);
-            firstLeftBarrier.SetActive(false);
-            secondLeftBarrier.SetActive(false);
-        }
-        else
-        {
-            theLeftWall.SetActive(true);
+            if (theEndPoint.gameObject.GetComponent<ElevatorBookends>().leftWall == true)
+            {
+                theLeftWall.SetActive(true);
+            }
+            else if (theEndPoint.gameObject.GetComponent<ElevatorBookends>().leftWall == false)
+            {
+                theLeftWall.SetActive(false);
+            }
+            if (theEndPoint.gameObject.GetComponent<ElevatorBookends>().rightWall == true)
+            {
+                theRightWall.SetActive(true);
+            }
+            else if (theEndPoint.gameObject.GetComponent<ElevatorBookends>().rightWall == false)
+            {
+                theRightWall.SetActive(false);
+            }
         }
 
         if (objectToMove.transform.position == startPoint.position)
         {
+            onEndPoint = false;
+            onStartPoint = true;
             if (waitCounter < 0f)
             {
                 reachedDestination = true;
+                theStartLeftBarrier.SetActive(false);
+                theStartRightBarrier.SetActive(false);
+                theEndLeftBarrier.SetActive(false);
+                theEndRightBarrier.SetActive(false);
             }
             notMoving = true;
             currentTarget = endPoint.position;
-            theRightWall.SetActive(false);
-            firstRightBarrier.SetActive(false);
-            secondRightBarrier.SetActive(false);
+            if (theStartPoint.gameObject.GetComponent<ElevatorBookends>().leftWall == true)
+            {
+                theLeftWall.SetActive(true);
+            }
+            else if (theStartPoint.gameObject.GetComponent<ElevatorBookends>().leftWall == false)
+            {
+                theLeftWall.SetActive(false);
+            }
+            if (theStartPoint.gameObject.GetComponent<ElevatorBookends>().rightWall == true)
+            {
+                theRightWall.SetActive(true);
+            }
+            else if (theStartPoint.gameObject.GetComponent<ElevatorBookends>().rightWall == false)
+            {
+                theRightWall.SetActive(false);
+            }
         }
-        else
+
+
+
+        if (onStartPoint)
         {
-            theRightWall.SetActive(true);
+            if (theEndPoint.gameObject.GetComponent<ElevatorBookends>().endLeftBarrier == true)
+            {
+                theEndLeftBarrier.SetActive(true);
+            }
+            else if (theEndPoint.gameObject.GetComponent<ElevatorBookends>().endLeftBarrier == false)
+            {
+                theEndLeftBarrier.SetActive(false);
+            }
+            if (theEndPoint.gameObject.GetComponent<ElevatorBookends>().endRightBarrier == true)
+            {
+                theEndRightBarrier.SetActive(true);
+            }
+            else if (theEndPoint.gameObject.GetComponent<ElevatorBookends>().endRightBarrier == false)
+            {
+                theEndRightBarrier.SetActive(false);
+            }
+        }
+
+
+
+        if (onEndPoint)
+        {
+            if (theStartPoint.gameObject.GetComponent<ElevatorBookends>().startLeftBarrier == true)
+            {
+                theStartLeftBarrier.SetActive(true);
+            }
+            else if (theStartPoint.gameObject.GetComponent<ElevatorBookends>().startLeftBarrier == false)
+            {
+                theStartLeftBarrier.SetActive(false);
+            }
+            if (theStartPoint.gameObject.GetComponent<ElevatorBookends>().startRightBarrier == true)
+            {
+                theStartRightBarrier.SetActive(true);
+            }
+            else if (theStartPoint.gameObject.GetComponent<ElevatorBookends>().startRightBarrier == false)
+            {
+                theStartRightBarrier.SetActive(false);
+            }
         }
 
 
@@ -86,11 +171,49 @@ public class AutoElevator : MonoBehaviour
 
 
 
+        if (!notMoving)
+        {
+            if (theStartPoint.gameObject.GetComponent<ElevatorBookends>().startLeftBarrier == true)
+            {
+                theStartLeftBarrier.SetActive(true);
+            }
+            else if (theStartPoint.gameObject.GetComponent<ElevatorBookends>().startLeftBarrier == false)
+            {
+                theStartLeftBarrier.SetActive(false);
+            }
+            if (theStartPoint.gameObject.GetComponent<ElevatorBookends>().startRightBarrier == true)
+            {
+                theStartRightBarrier.SetActive(true);
+            }
+            else if (theStartPoint.gameObject.GetComponent<ElevatorBookends>().startRightBarrier == false)
+            {
+                theStartRightBarrier.SetActive(false);
+            }
+            if (theEndPoint.gameObject.GetComponent<ElevatorBookends>().endLeftBarrier == true)
+            {
+                theEndLeftBarrier.SetActive(true);
+            }
+            else if (theEndPoint.gameObject.GetComponent<ElevatorBookends>().endLeftBarrier == false)
+            {
+                theEndLeftBarrier.SetActive(false);
+            }
+            if (theEndPoint.gameObject.GetComponent<ElevatorBookends>().endRightBarrier == true)
+            {
+                theEndRightBarrier.SetActive(true);
+            }
+            else if (theEndPoint.gameObject.GetComponent<ElevatorBookends>().endRightBarrier == false)
+            {
+                theEndRightBarrier.SetActive(false);
+            }
+        }
+
+
+
         if (waitCounter < 0f)
         {
             notMoving = false;
-            firstRightBarrier.SetActive(true);
-            firstLeftBarrier.SetActive(true);
+            theLeftWall.SetActive(true);
+            theRightWall.SetActive(true);
             objectToMove.transform.position = Vector3.MoveTowards(objectToMove.transform.position, currentTarget, moveSpeed * Time.deltaTime);
         }
     }
---
+++ unity-game/Assets/Scripts/ElevatorScripts/ElevatorBookends.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorBookends : MonoBehaviour {
+
+    public bool leftWall;
+    public bool rightWall;
+    public bool startLeftBarrier;
+    public bool startRightBarrier;
+    public bool endLeftBarrier;
+    public bool endRightBarrier;
+
+	void Start ()
+    {
+	
+	}
+
+
+
+    void Update ()
+    {
+	
+	}
+}
